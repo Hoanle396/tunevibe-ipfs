ef014f49d45a000959d11a6df1e9b2efdd654848
Lê Hữu Hoàn <hoanle396@gmail.com>
2024-05-08 22:32:37 +0700

    update port

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require("./src/configs/database").connect(); 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("./src/configs/database").connect();
 const { router } = require("./src/controllers");
-const swaggerDocs = require("./src/configs/swagger"); 
+const swaggerDocs = require("./src/configs/swagger");
 const { logger } = require("./src/utils/logger");
 
 app.use(cors({ origin: true, credentials: true }));
-app.use(bodyParser.urlencoded({extended: true}))
-const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 3001;
 
-swaggerDocs(app,port)
-app.use(router)
+swaggerDocs(app, port);
+app.use(router);
 app.listen(port, () => {
-  logger.star(`🚀 App listening on port ${port}`)
-})
+  logger.star(`🚀 App listening on port ${port}`);
+});
